c4612334ac05d1c52af13e5c891b870f57ee5b88
YusukeIkeda <yusuke_aono@yahoo.co.jp>
2024-05-02 22:13:40 +0900

    CRUD実装中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,22 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+    @book.year=2019
+  end
+
+  def create
+    book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
 end
 
 
